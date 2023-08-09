c4755e5800863946118afcbcc8097cdee6768746
Vamsi <vamsiparimi99@gmail.com>
2023-08-09 12:23:12 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const express=require('express')
+const axios=require('axios')
+const bodyParser=require('body-parser')
+const app=express()
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+const data=async(req,res)=>{
+try{
+
+    const urls=req.query.url
+    let numbers=[]
+    for(i in urls){
+    if(urls[i].startsWith('http://localhost:5500/')){
+    let values= await axios.get(urls[i])
+    let ans=values.data.numbers
+    numbers.push(...ans)
+    
+    }
+}
+    let set=new Set(numbers)
+    const sorted=Array.from(set).sort((a,b)=>a-b)
+    res.status(200).json({
+        "numbers":sorted
+    })
+
+}catch(err){
+    console.log(err);
+}
+}
+app.get('/numbers',data)
+app.listen(3000,console.log('listening'))
